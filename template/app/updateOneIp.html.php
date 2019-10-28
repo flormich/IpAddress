debc19705d85107a2b1c5bfe82cdd514da27709e
florian <michelflorian@yahoo.fr>
2019-10-28 16:13:50 +0100

    Add link machine

--- template/app/updateOneIp.html.php
+++ template/app/updateOneIp.html.php
@@ -127,6 +127,12 @@
 							<tr class="heightDelete">
 								<td>
 									<a href="/public/index.php/delete/<?= $ipAddress->getIp()?>"> <img class="icoDroit" src="/assets/img/Full Trash.png" alt="delete" title="Delete de <?= $ipAddress->getIp()?>" ></a>
+									<a></a>
+									<?php 
+										if ($ipAddress->getTypeMat() == 'Imprimante' OR ($ipAddress->getTypeMat() == 'SRV') OR ($ipAddress->getTypeMat() == 'Etiquette') OR ($ipAddress->getTypeMat() == 'BOX') OR ($ipAddress->getTypeMat() == 'Routeur') OR ($ipAddress->getTypeMat() == 'Switch')) {
+									?>
+											<a href="\\<?= $ipAddress->getIp()?>"> <img class="icoGauche" src="/assets/img/link.png" alt="edit" title="Lien vers <?= $ipAddress->getIp()?>" ></a>
+										<?php } else {} ?>
 								</td>
 							</tr>
 						</table>
